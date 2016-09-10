ee17539efc14e6645247b98a0835b998c427850d
Yonathan Ayenew <yonayen@gmail.com>
2016-09-10 14:23:23 -0700

    define counter 'i' and length

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ var Pool = (function()
       this._size = size;
       this._pointer =size;
       this._elements = [];
+
+      var i = 0;
+      var length - this._size;
+
+      for(i; i < length; ++i)
+      {
+        this._elements[i] = this._type.create();
+      }
     }
   }
 
